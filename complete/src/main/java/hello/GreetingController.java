12a9029d81bd5ade0e67894323b16a5a1329633e
jenn14108 <jenn.lee14108@gmail.com>
2019-08-07 10:57:00 +0800

    Fix numDays param

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.databind.*;
@@ -30,12 +31,13 @@ public class GreetingController {
     }
 
     //This is the url that is mapped to this method
-    @GetMapping(value = "/prices",
+    @GetMapping(value = "/prices/{numDays}",
             produces =  MediaType.APPLICATION_JSON_VALUE)
     //we want to take in two parameters - the ticker and the number of days of data to display
     public String prices(
             @RequestParam(value = "ticker" , defaultValue="DNKN") String ticker,
-            @RequestParam(value = "numDays", defaultValue="20") String numDays) throws IOException {
+            @PathVariable(required = false) Integer numDays) throws IOException {
+        // @RequestParam(value = "numDays", defaultValue="5") Integer numDays)
 
         //the alpha vantage querying url with ticker is a variable
         final String AlphaVantageUri = "https://www.alphavantage.co/query?" +
@@ -52,15 +54,15 @@ public class GreetingController {
         JsonNode rootNode = new ObjectMapper().readTree(initialRes.getBody());
         StringBuilder prices = new StringBuilder();
 
-        JsonNode metaData = rootNode.get("Meta Data");
-        prices.append(metaData.path("2. Symbol").asText()+"\n");
+        JsonNode metaDataParent = rootNode.path("Meta Data");
+        prices.append(metaDataParent.path("2. Symbol").asText()+"\n");
 
         //go into the main chunk of JSON containing all the dates
         JsonNode timeSeriesStart = rootNode.get("Time Series (Daily)");
         //create an iterator for all dates
         Iterator<String> dates = timeSeriesStart.fieldNames();
         Integer counter = 1;
-        while(counter <= Integer.parseInt(numDays) && dates.hasNext()){
+        while(counter <= numDays && dates.hasNext()){
             String date = dates.next();
             JsonNode fieldValue = timeSeriesStart.get(date);
             prices.append("\n" + date + " :\n");
